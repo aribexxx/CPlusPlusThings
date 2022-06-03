c0a4920f22917ff8a34922fc87fd69d92e036d1b
ziyuzhong <ziyuzhong@tencent.com>
2022-06-03 15:58:49 +0800

    virtual 用法

--- basic_content/abstract/derived_full.cpp
+++ basic_content/abstract/derived_full.cpp
@@ -5,7 +5,7 @@
  * @version v1
  * @date 2019-07-20
  */
-
+// 派生类必须实现抽象类的virtual function 才可以用，跟java不同（java 的子类可以直接call 父类的方法）
 #include<iostream> 
 using namespace std; 
 
@@ -14,19 +14,28 @@ class Base
     int x; 
     public: 
     virtual void fun() = 0; 
-    int getX() { return x; } 
+    virtual int getX() { return x; } 
 }; 
 
 class Derived: public Base 
 { 
+    public:
     int y; 
     public: 
     void fun() { cout << "fun() called"; }  // 实现了fun()函数
+    public:
+     int getX(){
+        return y;
+    }
 }; 
 
 int main(void) 
 { 
     Derived d; 
+    d.y=2000;
     d.fun(); 
+    int out=d.getX();
+    std::cout<<d.y<<std::endl;
+    std::cout<<out<<std::endl;
     return 0; 
 } 
