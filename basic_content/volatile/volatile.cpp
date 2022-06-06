d47ed9d19b021aa8bd57e8229eaec53e2ec2c080
ziyuzhong <ziyuzhong@tencent.com>
2022-06-06 21:28:08 +0800

    volatill VS no Volatile

--- basic_content/volatile/volatile.cpp
+++ basic_content/volatile/volatile.cpp
@@ -1,16 +1,19 @@
 /* Compile code with optimization option */
 #include <stdio.h> 
-
+//
 int main(void) 
-{ 
-    const volatile int local = 10; 
+{   //Initial value of local : 10 
+    //Modified value of local: 100 
+    const volatile int vol = 10; 
+    int * ptr_vol = (int*) & vol; 
+    printf("Initial value of vol : %d \n", vol); 
+    *ptr_vol = 100; 
+    printf("Modified value of vol: %d \n", vol); 
+//No volatile
+    const int local = 10; 
     int *ptr = (int*) &local; 
-
-    printf("Initial value of local : %d \n", local); 
-
+    printf("Initial value of local : %d \n", vol); 
     *ptr = 100; 
-
-    printf("Modified value of local: %d \n", local); 
-
+    printf("Modified value of local: %d \n", vol); 
     return 0; 
 } 
